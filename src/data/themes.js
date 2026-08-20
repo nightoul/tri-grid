d3fda94d39623e5d1c26092dcb401b460466fdb3
Jakub Jenč <62183088+nightoul@users.noreply.github.com>
2026-08-20 21:55:55 +0200

    refine themes

--- src/data/themes.js
+++ src/data/themes.js
@@ -1,10 +1,7 @@
 const themes = [
   { id: 'industrial', label: 'IND', name: 'Industrial' },
-  { id: 'workshop', label: 'WORK', name: 'Workshop' },
-  { id: 'workshop2', label: 'WORK2', name: 'Workshop 2' },
   { id: 'sunrise', label: 'SUN', name: 'Sunrise' },
   { id: 'sunrise2', label: 'SUN2', name: 'Sunrise 2' },
-  { id: 'reverse', label: 'REV', name: 'Reverse' },
   { id: 'crimson', label: 'RED', name: 'Crimson' },
 ]
 
