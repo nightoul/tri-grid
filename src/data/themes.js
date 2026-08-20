fd793b097eeb7d3e68f2e85bd0c4da2c4eedf8ce
Jakub Jenč <62183088+nightoul@users.noreply.github.com>
2026-08-20 21:08:33 +0200

    change bg hero

--- src/data/themes.js
+++ src/data/themes.js
@@ -1,15 +1,11 @@
 const themes = [
   { id: 'industrial', label: 'IND', name: 'Industrial' },
-  { id: 'industrial2', label: 'IND2', name: 'Industrial 2' },
-  { id: 'industrial3', label: 'IND3', name: 'Industrial 3' },
   { id: 'workshop', label: 'WORK', name: 'Workshop' },
   { id: 'workshop2', label: 'WORK2', name: 'Workshop 2' },
   { id: 'sunrise', label: 'SUN', name: 'Sunrise' },
   { id: 'sunrise2', label: 'SUN2', name: 'Sunrise 2' },
   { id: 'reverse', label: 'REV', name: 'Reverse' },
-  { id: 'terracotta', label: 'TERRA', name: 'Terracotta' },
   { id: 'crimson', label: 'RED', name: 'Crimson' },
-  { id: 'meadow', label: 'MEAD', name: 'Meadow' },
 ]
 
 export default themes
