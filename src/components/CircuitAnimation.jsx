c7dc2931b6780d46c925b8f6095b1a8181195b90
Jakub Jenč <62183088+nightoul@users.noreply.github.com>
2026-08-21 13:44:00 +0200

    animation

--- src/components/CircuitAnimation.jsx
+++ src/components/CircuitAnimation.jsx
@@ -17,7 +17,10 @@ function CircuitAnimation() {
       </defs>
 
       <g className="circuit-animation__traces">
-        <path id="circuitRoute" d="M-80 185 H190 V112 H490 V218 H790 V125 H1080 V245 H1520" />
+        <path
+          id="circuitRoute"
+          d="M-80 185 H190 V112 H490 V218 H790 V125 H1080 V245 H1400 V625 H1120 V680 H820 V610 H500 V670 H210 V625 H-80"
+        />
         <path d="M490 218 V390 H720" />
         <path d="M1080 245 V430 H1300" />
       </g>
@@ -29,12 +32,17 @@ function CircuitAnimation() {
         <circle cx="1080" cy="245" r="5" />
         <circle cx="720" cy="390" r="5" />
         <circle cx="1300" cy="430" r="5" />
+        <circle cx="1400" cy="625" r="5" />
+        <circle cx="1120" cy="680" r="5" />
+        <circle cx="820" cy="610" r="5" />
+        <circle cx="500" cy="670" r="5" />
+        <circle cx="210" cy="625" r="5" />
       </g>
 
       <g className="circuit-animation__spark" filter="url(#circuitSparkGlow)">
         <circle r="10" className="circuit-animation__spark-halo" />
         <circle r="3.5" className="circuit-animation__spark-core" />
-        <animateMotion dur="11s" repeatCount="indefinite">
+        <animateMotion dur="19s" repeatCount="indefinite">
           <mpath href="#circuitRoute" />
         </animateMotion>
       </g>
